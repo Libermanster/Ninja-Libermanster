97ef1e6b51126a06fac53b4837248d29928f86ee
Libermanster <yonashliberman@gmail.com>
2022-08-18 19:47:55 +0300

    deleting text engine and merging it inside util

--- util.h
+++ util.h
@@ -45,3 +45,34 @@ char *stringConnect(char *s1, char *s2);
 void deleteSpacesAtEnd(char *s);
 
 
+
+/*
+    breif: get back the label
+    input: line[81], label[]
+    output: void, change the label char array into the label
+*/
+void getLabelName(char*, char*);
+/*
+    breif: checks if a line contain only whitespaces, works only for for 81 digits lines
+    input: line[81]
+    output: 1 for true 0 for false
+*/
+int isEmpty(char*);
+/*
+    breif: checks for comments line
+    input: line[81]
+    output: 1 for true 0 for false
+*/
+int isComment(char*);
+/*
+    breif: checks for lines thats starts with label
+    input: line[81]
+    output: 1 for true 0 for false
+*/
+int starts_with_label(char*);
+/*
+    breif: checks for lines thats starts with label, contain only whitespaces or comments line
+    input: line[81]
+    output: 1 for empty line 2 for comment line 3 for labeled line 0 for else
+*/
+int throughText(char*);
