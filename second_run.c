d13eb32ee809aa32d3c88b757b963fe7459204df
Ninja <davidsmirin@gmail.com>
2022-08-19 18:35:53 +0300

    second run

--- second_run.c
+++ second_run.c
@@ -4,13 +4,13 @@ inctractionArray second_run(inctractionArray* a, dataImage* d, symbolList* sl) {
     updateDataSymbolsInList(sl,a);
 
     
-    for(i=0;i<a->ic;i++) {
+    for(int i=0;i<a->ic;i++) {
         if(a->arr[i].labelSwitch==1) 
         {
-            if(labelExsits(a->arr[i].format.labelName)==1) 
+            if(labelExsits(a->arr[i].format.formatType.labelName)==1) 
             {
-                fillArray(getSymbolAdress(a->arr[i].format.labelName),8,a->arr[a->ic].format.formatType.dataFormat.data);
-                fillArray(getSymbolSource(a->arr[i].format.labelName),2,a->arr[a->ic].format.formatType.dataFormat.are);
+                fillArray(getSymbolAdress(a->arr[i].format.formatType.labelName,sl),8,a->arr[a->ic].format.formatType.dataFormat.data);
+                fillArray(getSymbolSource(a->arr[i].format.formatType.labelName,sl),2,a->arr[a->ic].format.formatType.dataFormat.are);
             }
             else     
             {
