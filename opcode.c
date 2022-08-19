8dcc4ff7017b5a6d075cbeed4d48daa863c204a0
Libermanster <yonashliberman@gmail.com>
2022-08-19 20:41:11 +0300

    suicide

--- opcode.c
+++ opcode.c
@@ -2,8 +2,9 @@
 const char *opcodeArray[16]={"mov","cmp","add","sub","not","clr","lea","inc","dec","jmp","bne","get","prn","jsr","rts","hlt"};
 
 int getOpcode(char* str) {
-    for(int i=0;i<=15;i++) {
-        if(strcmp(str,opcodeArray[i])==0);
+    int i=0;
+    for(;i<=15;i++) {
+        if(strcmp(str,opcodeArray[i])==0)
             return i;
     }
     return -1;
