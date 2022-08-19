7533c6921ee94c11136bfdfd5ee6ba7026f5ea19
Libermanster <yonashliberman@gmail.com>
2022-08-19 13:51:42 +0300

    some fixes in comments

--- operand.h
+++ operand.h
@@ -1,12 +1,12 @@
 #ifndef OPERAND
 #define OPERAND
 typedef struct operand {
-    int reffMode; //שיטת המיעון, refferenc Mode
+    int reffMode; /* //שיטת המיעון, refferenc Mode */
     value value;
 } operand;
 
 typedef union value {
-    int number; //data ot register number
+    int number;/*  //data ot register number */
     char *labelName;
     struct labelNameAndNum {
         char *name;
