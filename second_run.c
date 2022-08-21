b5f08653b69aaa4d9649850d3c96833587f3b282
Ninja <davidsmirin@gmail.com>
2022-08-21 20:34:19 +0300

    niga

--- second_run.c
+++ second_run.c
@@ -10,8 +10,8 @@ void second_run(inctractionArray* a, dataImage* d, symbolList* sl) {
         {
             if(isLabelExsits(a->arr[i].format.formatType.labelName,sl)==1) 
             {
-                decToBin(getSymbolAdress(a->arr[i].format.formatType.labelName,sl),8,a->arr[a->ic].format.formatType.dataFormat.data);
-                decToBin(getSymbolSource(a->arr[i].format.formatType.labelName,sl),2,a->arr[a->ic].format.formatType.dataFormat.are);
+                decToBin(getSymbolAdress(a->arr[i].format.formatType.labelName,sl),8,a->arr[i].format.formatType.dataFormat.data);
+                decToBin(getSymbolSource(a->arr[i].format.formatType.labelName,sl),2,a->arr[i].format.formatType.dataFormat.are);
                 a->arr[i].format.formatTypeIndicator = 3;
             }
             /*     
