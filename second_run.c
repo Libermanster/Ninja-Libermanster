6ce81065c3802c735b216e3379f979a101d09a9a
Ninja <davidsmirin@gmail.com>
2022-08-21 20:41:42 +0300

    n

--- second_run.c
+++ second_run.c
@@ -2,18 +2,19 @@
 
 void second_run(inctractionArray* a, dataImage* d, symbolList* sl) {
     int i;
+    i=0;
     updateSymbolsInList(sl,a);
 
 
-    for(i=0;i<a->ic;i++) {
+    for(;i<a->ic;i++) {
         if(a->arr[i].labelSwitch==1) 
         {
-            if(isLabelExsits(a->arr[i].format.formatType.labelName,sl)==1) 
-            {
+            /*if(isLabelExsits(a->arr[i].format.formatType.labelName,sl)==1) 
+            {*/
                 decToBin(getSymbolAdress(a->arr[i].format.formatType.labelName,sl),8,a->arr[i].format.formatType.dataFormat.data);
                 decToBin(getSymbolSource(a->arr[i].format.formatType.labelName,sl),2,a->arr[i].format.formatType.dataFormat.are);
                 a->arr[i].format.formatTypeIndicator = 3;
-            }
+           /* } */
             /*     
             {
                 error: label doenst exisits 
